96ab162cd4e4fbd301f143cd2281e72b6b02a8a7
Dimasik <dimakadenuk2000@gmail.com>
2025-07-15 22:59:12 +0300

    del

--- assets/js/accordion.js
+++
@@ -1,9 +0,0 @@
-let accordionSection = document.querySelector('.accordion');
-if (accordionSection) {
-  //Acodron
-  document.querySelectorAll(".accordion-item").forEach((item) => {
-    item.querySelector(".accordion-item-header").addEventListener("click", () => {
-      item.classList.toggle("open");
-    });
-  });
-}
